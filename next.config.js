da2e698fb7259822686bbd1f43ca366ff180188d
Abulele Maqungo <abu.maqungo@gmail.com>
2025-09-09 03:10:31 +0200

    Try minimal config without static export

--- next.config.js
+++ next.config.js
@@ -1,10 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
+  // Remove output: 'export' temporarily to test
   images: {
     unoptimized: true
-  },
-  trailingSlash: true,
+  }
 }
 
 module.exports = nextConfig
